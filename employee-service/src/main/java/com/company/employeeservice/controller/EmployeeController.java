e97df0b395dc2909c36c7fb2d7cc36780d625327
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-10 17:14:16 +0400

    Added EmployeeController

---
+++ employee-service/src/main/java/com/company/employeeservice/controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.company.employeeservice.controller;
+
+import com.company.employeeservice.service.EmployeeService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/employee")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+}
