07d136beb91f99388513f7807d9409ea57a0c2d4
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-10 17:16:10 +0400

    Added EmployeeService

--- employee-service/src/main/java/com/company/employeeservice/service/EmployeeService.java
+++ employee-service/src/main/java/com/company/employeeservice/service/EmployeeService.java
@@ -1,7 +1,14 @@
 package com.company.employeeservice.service;
 
+import com.company.employeeservice.repository.EmployeeRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
 }
