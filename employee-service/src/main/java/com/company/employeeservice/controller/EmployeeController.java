de4eb0787ac18fa0ca2a8e721098b1a25d0bfa62
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-11 12:27:15 +0400

    initial commit

--- employee-service/src/main/java/com/company/employeeservice/controller/EmployeeController.java
+++ employee-service/src/main/java/com/company/employeeservice/controller/EmployeeController.java
@@ -1,16 +1,47 @@
 package com.company.employeeservice.controller;
 
-import com.company.employeeservice.service.EmployeeService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.company.employeeservice.model.Employee;
+import com.company.employeeservice.repository.EmployeeRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/employee")
 public class EmployeeController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeController.class);
 
-    private final EmployeeService employeeService;
+    private final EmployeeRepository repository;
 
-    public EmployeeController(EmployeeService employeeService) {
-        this.employeeService = employeeService;
+    public EmployeeController(EmployeeRepository repository) {
+        this.repository = repository;
     }
+
+    @PostMapping
+    public Employee add(@RequestBody Employee employee) {
+        LOGGER.info("Employee add: {}", employee);
+        return repository.add(employee);
+    }
+
+    @GetMapping
+    public List<Employee> findAll() {
+        LOGGER.info("Employee find");
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Employee findById(@PathVariable("id") Long id) {
+        LOGGER.info("Employee find: id={}", id);
+        return repository.findById(id);
+    }
+
+    @GetMapping("/department/{departmentId}")
+    public List<Employee> findByDepartment(@PathVariable("departmentId") Long departmentId) {
+        LOGGER.info("Employee find: departmentId={}", departmentId);
+        return repository.findByDepartment(departmentId);
+    }
+
 }
