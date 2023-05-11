c6c6b62eb335fecce155681dd1b81aed072936ce
ibrahimqarayev <qarayevibrahim7@mail.ru>
2023-05-11 12:27:19 +0400

    initial commit

--- department-service/src/main/java/com/company/departmentservice/repository/DepartmentRepository.java
+++ department-service/src/main/java/com/company/departmentservice/repository/DepartmentRepository.java
@@ -1,9 +1,30 @@
 package com.company.departmentservice.repository;
 
-
 import com.company.departmentservice.model.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.ArrayList;
+import java.util.List;
 
-public interface DepartmentRepository extends JpaRepository<Department, Long> {
+@Repository
+public class DepartmentRepository {
+    private List<Department> departments
+            = new ArrayList<>();
+
+    public Department addDepartment(Department department) {
+        departments.add(department);
+        return department;
+    }
+
+    public Department findById(Long id) {
+        return departments.stream()
+                .filter(department ->
+                        department.getId().equals(id))
+                .findFirst()
+                .orElseThrow();
+    }
+
+    public List<Department> findAll() {
+        return departments;
+    }
 
 }
